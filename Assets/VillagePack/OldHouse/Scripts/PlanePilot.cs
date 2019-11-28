5fd9f552a1b1e48f1afcf335dd53be24affc6630
JosephRutgers42 <55005555+JosephRutgers42@users.noreply.github.com>
2019-11-27 19:26:58 -0500

    Balls Added

--- Assets/VillagePack/OldHouse/Scripts/PlanePilot.cs
+++ Assets/VillagePack/OldHouse/Scripts/PlanePilot.cs
@@ -14,12 +14,12 @@ public class PlanePilot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveCamTo = transform.position - transform.forward * 10.0f + Vector3.up * 5.0f;
+        Vector3 moveCamTo = transform.position - transform.forward * 0.5f + Vector3.up * 0.5f;
         float bias = 0.96f;
         Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1.0f - bias);
-        Camera.main.transform.LookAt(transform.position + transform.forward * 30.0f);
+        Camera.main.transform.LookAt(transform.position + transform.forward * 10.0f);
         transform.position += transform.forward * Time.deltaTime * speed;
-        speed -= transform.forward.y * Time.deltaTime * 30.0f;
+        speed -= transform.forward.y * Time.deltaTime * 10.0f;
 
         if (speed < 35.0f) {
             speed = 35.0f;
